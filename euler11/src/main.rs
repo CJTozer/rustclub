9ee11533f5c252a966f2086c7048f9a1b4ccf3aa
Chris Tozer <christopherjtozer@gmail.com>
2015-05-16 23:06:13 +0000

    Initial commit - basic cargo project

---
+++ euler11/src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    println!("Enter something");
+    io::stdin().read_line(&mut input)
+        .ok()
+        .expect("Couldn't read line");
+    println!("You input '{}'", input);
+}
