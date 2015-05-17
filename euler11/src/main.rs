e45cf8d9dcc01237ae8b5559da485322e4359dd2
Chris Tozer <christopherjtozer@gmail.com>
2015-05-17 14:36:15 +0000

    Loading data into grid

--- euler11/src/main.rs
+++ euler11/src/main.rs
@@ -1,21 +1,12 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::path::Path;
 
 fn main() {
-    // ## This should look like:
-    // ## - let mut grid = Grid::new("data/grid");
-    // ## - grid.solve();
-    let mut grid: Grid;
-    let mut data = String::new();
-    read_data(&mut data);
-}
-
-fn read_data(data: &mut String) {
-    // ## unwrap() acts on Result; means panic on fail...
-    // ## ...but not with a terribly useful output
-    let mut file = File::open("data/grid").unwrap();
-    file.read_to_string(data).unwrap();
-    println!("We read data: {}", data);
+    let data_path = Path::new("data/grid");
+    let grid: Grid = Grid::from_file(data_path);
+    grid.solve();
 }
 
 struct Grid {
@@ -29,12 +20,22 @@ impl Grid {
         }
     }
 
-    fn from_file(data_file: String) -> Grid {
+    fn from_file(path: &Path) -> Grid {
         let mut g = Grid::new();
-        let mut file = File::open(data_file).unwrap();
-        let mut data = String::new();
-        file.read_to_string(&mut data).unwrap();
-        println!("We read data: {}", data);
+        let file = File::open(path).unwrap();
+        let buf = BufReader::new(file);
+        for line in buf.lines() {
+            let mut row = Vec::new();
+            for cell in line.unwrap().split(" ") {
+                println!("Cell: {}", cell);
+                row.push(cell.parse::<u32>().unwrap());
+            }
+            g.cells.push(row);
+        }
+        //let mut file = File::open(path).unwrap();
+        //let mut data = String::new();
+        //file.read_to_string(&mut data).unwrap();
+        // println!("We read data: {}", data);
         // ## Need to put the data into the cells...
         g
     }
