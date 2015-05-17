707da060b3a442d7118a96d702d816b385e218fa
Chris Tozer <christopherjtozer@gmail.com>
2015-05-17 11:58:02 +0000

    Refactor, fix warnings

--- euler11/src/main.rs
+++ euler11/src/main.rs
@@ -2,10 +2,13 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
+    let mut data = String::new();
+    read_data(&mut data);
+}
+
+fn read_data(data: &mut String) {
     // ## unwrap() means panic on fail - but not a useful output...
     let mut file = File::open("data/grid").unwrap();
-
-    let data = &mut String::new();
-    file.read_to_string(data);
+    file.read_to_string(data).unwrap();
     println!("We read data: {}", data);
 }
