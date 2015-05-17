1f2885b4747e0a927b58e5ff0e40216ecf51b0e1
Chris Tozer <christopherjtozer@gmail.com>
2015-05-17 11:46:21 +0000

    Read complete file

--- euler11/src/main.rs
+++ euler11/src/main.rs
@@ -1,10 +1,11 @@
-use std::io;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
-    let mut input = String::new();
-    println!("Enter something");
-    io::stdin().read_line(&mut input)
-        .ok()
-        .expect("Couldn't read line");
-    println!("You input '{}'", input);
+    // ## unwrap() means panic on fail - but not a useful output...
+    let mut file = File::open("data/grid").unwrap();
+
+    let data = &mut String::new();
+    file.read_to_string(data);
+    println!("We read data: {}", data);
 }
