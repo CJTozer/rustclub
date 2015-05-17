266dc19cb2853ca5929a899a627895ca4da5256c
Chris Tozer <christopherjtozer@gmail.com>
2015-05-17 12:24:04 +0000

    Basic framework for Grid

--- euler11/src/main.rs
+++ euler11/src/main.rs
@@ -2,13 +2,32 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
+    // ## This should look like:
+    // ## - let mut grid = Grid::new("data/grid");
+    // ## - grid.solve();
+    let mut grid: Grid;
     let mut data = String::new();
     read_data(&mut data);
 }
 
 fn read_data(data: &mut String) {
-    // ## unwrap() means panic on fail - but not a useful output...
+    // ## unwrap() acts on Result; means panic on fail...
+    // ## ...but not with a terribly useful output
     let mut file = File::open("data/grid").unwrap();
     file.read_to_string(data).unwrap();
     println!("We read data: {}", data);
+}
+
+struct Grid {
+    cells: Vec<Vec<u32>>,
+}
+
+impl Grid {
+    fn new(file: String) -> Grid {
+        Grid { cells: Vec::new() }
+    }
+
+    fn solve(&self) {
+        println!("Grid.solve not implemented yet");
+    }
 }
