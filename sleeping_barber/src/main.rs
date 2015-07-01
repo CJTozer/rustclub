feb5de1d16826f8f311289e2b6d32ef37330cd94
Chris Tozer <christopherjtozer@gmail.com>
2015-07-01 21:13:14 +0000

    There's a shop, still no customers

--- sleeping_barber/src/main.rs
+++ sleeping_barber/src/main.rs
@@ -1,19 +1,33 @@
 use std::thread;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     println!("Begin!");
-    thread::spawn(move || { barber() } );
+    let mut shop = Arc::new(Mutex::new(BarberShop{
+                                           free_waiting_seats: 5,
+                                           customer_queue: Vec::new() }));
+    thread::spawn(move || { barber(shop.clone()) } );
 
-    thread::sleep_ms(5000);
+    thread::sleep_ms(2000);
     println!("Done!");
 }
 
-fn barber() {
+fn barber(shop: Arc<Mutex<BarberShop>>) {
     println!("Barber arrives at work");
     loop {
+       println!("Barber checks queue");
+       let mut queue = shop.lock().unwrap();
+       if (queue.customer_queue.is_empty()) {
+         println!("No customers waiting");
+       }
+
        println!("Barber going for a nap");
        thread::sleep_ms(500);
        println!("Barber wakes up");
     }
 }
 
+struct BarberShop {
+    free_waiting_seats: u32,
+    customer_queue: Vec<u32>,
+}
