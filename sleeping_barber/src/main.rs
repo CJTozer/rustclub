2a0d0212b49acc6bf419bf8fa5c46d0c76ca672a
Chris Tozer <christopherjtozer@gmail.com>
2015-07-01 21:37:23 +0000

    Customers having a look

--- sleeping_barber/src/main.rs
+++ sleeping_barber/src/main.rs
@@ -6,7 +6,9 @@ fn main() {
     let mut shop = Arc::new(Mutex::new(BarberShop{
                                            free_waiting_seats: 5,
                                            customer_queue: Vec::new() }));
-    thread::spawn(move || { barber(shop.clone()) } );
+    let shop_copy = shop.clone();
+    thread::spawn(move || { barber(shop_copy) } );
+    thread::spawn(move || { customers(shop.clone()) } );
 
     thread::sleep_ms(2000);
     println!("Done!");
@@ -20,17 +22,36 @@ fn barber(shop_arc: Arc<Mutex<BarberShop>>) {
         match shop.customer_queue.pop() {
             None => {
                 println!("No customers waiting");
+                drop(shop);
                 println!("Barber going for a nap");
                 thread::sleep_ms(500);
                 println!("Barber wakes up");
             },
             Some(cust) => {
                 println!("Cutting customer {}'s hair", cust);
+                shop.free_waiting_seats += 1;
+                drop(shop);
                 thread::sleep_ms(100);
                 println!("Done cutting");
             }
         }
-        drop(shop);
+    }
+}
+
+fn customers(shop_arc: Arc<Mutex<BarberShop>>) {
+    println!("Customers start heading to the barbers");
+    let mut cust_no = 1;
+    loop {
+        let mut shop_copy = shop_arc.clone();
+        let this_cust = cust_no;
+        thread::spawn(move || {
+            println!("Customer {} enters shop", this_cust);
+            let mut shop = shop_copy.lock().unwrap();
+            println!("Spare seats = {}", shop.free_waiting_seats);
+
+        });
+
+        thread::sleep_ms(100);
     }
 }
 
